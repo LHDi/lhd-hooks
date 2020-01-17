8962ad434ecf7f3cd2d0aa17420e7b1a10314336
LHDD <elhadi98@gmail.com>
2020-01-17 11:18:25 +0100

    not working

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import useOnClickOutside from './hooks/useOnClickOutside'
-import useSwipe from './hooks/useSwipe'
-import useScrollAnimation from './hooks/useScrollAnimation'
-
-export {
-  useOnClickOutside,
-  useSwipe,
-  useScrollAnimation
-}
